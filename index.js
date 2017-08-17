7695ab9d22a88636834acc32d05fce1c21989364
Naman Kathuria <namankathuria@hotmail.com>
2017-08-17 11:43:04 +0530

    updated the namespaces

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var fs          = require('fs');
 var bitbucket = require('bitbucketjs');
 
 var files = require('./lib/files');
-var connect = require('./lib/connect');
+var vendors = require('./lib/vendors');
 var prefs = new Preferences('easy-init');
 
 clear();
@@ -33,16 +33,16 @@ var status = new Spinner('Authenticating you, please wait...');
 
 if (prefs.credentials) {
     status.start();
-    connect.connect(status, prefs.credentials).then(function(bb){
+    vendors.connect(status, prefs.credentials).then(function(bb){
         console.log(bb);
     },function(error){
           console.log('error', error); 
      });
 }else{
-    connect.getCredentials(status).then(function(params){
-        connect.connect(status, params).then(function(bb){
+    vendors.getCredentials(status).then(function(params){
+        vendors.connect(status, params).then(function(bb){
             var createStatus = new Spinner('Creating Repo.....');
-            connect.createRepoOnline(params.username, bb, createStatus).then(function(repo){
+            vendors.createRepoOnline(params.username, bb, createStatus).then(function(repo){
                 var fileList = _.without(fs.readdirSync('.'), '.git', '.gitignore');
                 files.createGitignore(fileList).then(function(){
                     git
