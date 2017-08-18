e2a9de868cb54ece5a91f03409aaaf12d87bc32d
Naman Kathuria <namankathuria@hotmail.com>
2017-08-18 14:11:04 +0530

    changed font to Slant

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var prefs = new Preferences('turbo-init');
 clear();
 console.log(
   chalk.yellow(
-    figlet.textSync('Turbo-Init', { horizontalLayout: 'full', font: 'Doom' })
+    figlet.textSync('Turbo-Init', { horizontalLayout: 'full', font: 'Slant' })
     )
 );
 
